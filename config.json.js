6ce2c1e126c2d133ddf015f4e87997f62827da11
Kenney <zql254@vip.qq.com>
2017-05-06 10:40:23 +0800

    add info softcenter

---
+++ config.json.js
@@ -0,0 +1,8 @@
+{
+    "description": "超酷的远程下载工具", 
+    "home_url": "Module_aria2.asp", 
+    "md5": "fa0d9e6116263c352b3fbb9e2ca40a00", 
+    "module": "aria2", 
+    "title": "aria2", 
+    "version": "0.0.1"
+}
