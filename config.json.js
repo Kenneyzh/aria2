ad840ffe610b9d662b665084eff3d9ea3c63c63f
Kenney <zql254@vip.qq.com>
2017-05-09 17:10:31 +0800

    add bt option

--- config.json.js
+++ config.json.js
@@ -1,11 +1,11 @@
 {
-    "build_date": "2017-05-06 13:37:20", 
+    "build_date": "2017-05-09 17:10:12", 
     "description": "超酷的远程下载工具", 
     "home_url": "Module_aria2.asp", 
-    "md5": "407b730ef49dda6d591b9f6d3f2fe57f", 
+    "md5": "2f1033d71fb36a8a18e1098af5cc6458", 
     "module": "aria2", 
     "name": "aria2", 
     "tar_url": "aria2/aria2.tar.gz", 
     "title": "aria2", 
-    "version": "1.0.1"
+    "version": "1.0.2"
 }
