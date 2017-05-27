efb155819a87f2b84f3a65752ef2115c2046bcee
Kenney <zql254@vip.qq.com>
2017-05-27 17:27:02 +0800

    fix auto start

--- config.json.js
+++ config.json.js
@@ -1,11 +1,11 @@
 {
-    "build_date": "2017-05-24 19:01:03", 
+    "build_date": "2017-05-27 17:26:51", 
     "description": "超酷的远程下载工具", 
     "home_url": "Module_aria2.asp", 
-    "md5": "f15d2d364a66c8d27e37f1f034ff968b", 
+    "md5": "0c26a12bb5431fccd395cc4970a6fa5b", 
     "module": "aria2", 
     "name": "aria2", 
     "tar_url": "aria2/aria2.tar.gz", 
     "title": "aria2", 
-    "version": "1.0.3"
+    "version": "1.0.4"
 }
