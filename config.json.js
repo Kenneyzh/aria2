242a06b08b1aeb7b8a43b0a967e4ceade5c0fcd2
Kenney <zql254@vip.qq.com>
2017-06-01 13:11:48 +0800

    fix

--- config.json.js
+++ config.json.js
@@ -1,11 +1,11 @@
 {
-    "build_date": "2017-06-01 13:04:28", 
+    "build_date": "2017-06-01 13:11:38", 
     "description": "超酷的远程下载工具", 
     "home_url": "Module_aria2.asp", 
-    "md5": "5f63a3c765db8112d0c179c811757477", 
+    "md5": "12e229257791d3617ab8ce5dabfe9b38", 
     "module": "aria2", 
     "name": "aria2", 
     "tar_url": "aria2/aria2.tar.gz", 
     "title": "aria2", 
-    "version": "1.0.5"
+    "version": "1.0.6"
 }
