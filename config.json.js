3dffa414abe083bb4e2b0c28490bf43aa10dd2e3
Kenney <zql254@vip.qq.com>
2017-06-05 12:31:02 +0800

    update aria2c

--- config.json.js
+++ config.json.js
@@ -1,11 +1,11 @@
 {
-    "build_date": "2017-06-01 13:11:38", 
+    "build_date": "2017-06-05 12:30:46", 
     "description": "超酷的远程下载工具", 
     "home_url": "Module_aria2.asp", 
-    "md5": "12e229257791d3617ab8ce5dabfe9b38", 
+    "md5": "cf58a9b32f2a4eed48ecaf31c9e44968", 
     "module": "aria2", 
     "name": "aria2", 
     "tar_url": "aria2/aria2.tar.gz", 
     "title": "aria2", 
-    "version": "1.0.6"
+    "version": "1.0.7"
 }
