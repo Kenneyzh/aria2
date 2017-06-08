7ee521b01b19b06ea7f088ac0f430bf589b67991
Kenney <zql254@vip.qq.com>
2017-06-08 11:28:58 +0800

    change logo

--- config.json.js
+++ config.json.js
@@ -1,11 +1,11 @@
 {
-    "build_date": "2017-06-06 19:51:26", 
+    "build_date": "2017-06-08 11:28:47", 
     "description": "超酷的远程下载工具", 
     "home_url": "Module_aria2.asp", 
-    "md5": "0e544b82cf2fb5f8178859373d192bb2", 
+    "md5": "ed6b6df6747ec94bdc61330458653f8b", 
     "module": "aria2", 
     "name": "aria2", 
     "tar_url": "aria2/aria2.tar.gz", 
     "title": "aria2", 
-    "version": "1.0.9"
+    "version": "1.1.0"
 }
